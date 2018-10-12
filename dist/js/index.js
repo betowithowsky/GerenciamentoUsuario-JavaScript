4bb482f8785f8bf69a6cca3620bc575147d1319e
= <betowithowsky@gmail.com>
2018-10-12 20:00:04 -0300

    enviando primeira vez

---
+++ dist/js/index.js
@@ -0,0 +1,18 @@
+
+/*var name = document.querySelector('#exampleInputName');
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector('#exampleInputBirth');
+var country = document.querySelector('#exampleInputCountry');
+var email = document.querySelector('#exampleInputEmail');
+var password = document.querySelector('#exampleInputPassword');
+var photo = document.querySelector('#exampleInputFile');
+var admin = document.querySelector('#exampleInputAdmin');*/
+
+/*document.querySelector("button").forEach(function(){
+    this.addEventListener('click', function(){
+
+    })
+});*/
+
+
+let userController = new UserController("form-user-create", "form-user-update", "table-users");
